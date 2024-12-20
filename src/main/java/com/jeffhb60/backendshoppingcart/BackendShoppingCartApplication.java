41df54f97c486ea6d1d9d30314a9dc6fa512db85
Jeff Butt <jbutt2@wgu.edu>
2024-12-20 12:10:31 -0500

    Initial Commit. Created Spring Boot Project with the following dependencies: - spring-boot-starter-data-jpa - spring-boot-starter-data-rest - spring-boot-starter-validation - spring-boot-starter-web - spring-boot-starter-web-services - com.h2database - spring-boot-starter-test

---
+++ src/main/java/com/jeffhb60/backendshoppingcart/BackendShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.backendshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShoppingCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffhb60/backendshoppingcart/BackendShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.backendshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
